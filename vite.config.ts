22fa4d7aec0aeb2ada6e06cb998d324f8dfe4754
Bian-Mu <2570772184@qq.com>
2025-04-14 11:52:33 +0800

    feat:initial finish

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import electron from 'vite-plugin-electron'
 
 export default defineConfig({
   plugins: [
     react(),
-    electron({
-      entry: 'electron/main.ts', // Electron 主进程入口文件
-    }),
   ],
-  base: './', // 确保静态资源路径正确
+  base: './',
   build: {
-    outDir: 'dist', // 打包输出目录
+    outDir: 'dist-react',
   }
 })
